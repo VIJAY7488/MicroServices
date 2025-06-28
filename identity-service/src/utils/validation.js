75c5eb50da80c14959b6ae2a3f636cdfe7e0b9d8
Vijay Patel <vijaypatel3207@gmail.com>
2025-06-28 10:42:48 +0530

    user validation is done

---
+++ identity-service/src/utils/validation.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+const validateUserRegistration = (data) => {
+    const schema = Joi.object({
+        username: Joi.string().min(3).max(30).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+
+    });
+
+    return schema.validate(data);
+};
+
+module.exports = {
+    validateUserRegistration,
+};
