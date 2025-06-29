a3e8760a7a0b144439cad31d4ece0303f65efb8c
Vijay Patel <vijaypatel3207@gmail.com>
2025-06-29 16:22:42 +0530

    user login controller is done

--- identity-service/src/controllers/identityController.js
+++ identity-service/src/controllers/identityController.js
@@ -1,6 +1,7 @@
 const generateToken = require('../utils/genrateToken');
 const logger = require('../utils/logger');
-const { validateUserRegistration } = require('../utils/validation');
+const { validateUserRegistration, validateUserLogin } = require('../utils/validation');
+const User = require('../models/User');
 
 // User Register 
 const registerUser = async (req, res) => {
@@ -40,8 +41,54 @@ const registerUser = async (req, res) => {
     }
 };
 
+// User Login
+const loginUser = async (req, res) => {
+    logger.info('Login endpoint hit');
+
+    try {
+        const { email, password } = req.body;
+
+        // Validate the Schema
+        const { errorr } = validateUserLogin(req.body);
+        if (errorr) {
+            logger.error(`Validation Error: ${errorr.details[0].message}`);
+            return res.status(400).json({ success: false, message: errorr.details[0].message });    
+        }
+
+        // Check if user exists
+        const user = await User.find({ email });
+        if (!user) {
+            logger.error('User not found');
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+
+        // Check password
+        const isPasswordValid = await user.comparePassword(password);
+        if (!isPasswordValid) {
+            logger.error('Invalid password or email');
+            return res.status(401).json({ success: false, message: 'Invalid password' });
+        }
+
+        // Generate tokens
+        const { accessToken, refreshToken } = await generateToken(user);
+
+        logger.info('User logged in successfully');
+        return res.status(200).json({ 
+            success: true, 
+            message: 'User logged in successfully',
+            accessToken,
+            refreshToken,
+            userId: user._id,
+        });
+    } catch (error) {
+        logger.error(`Error in loginUser: ${error.message}`);
+        return res.status(500).json({ success: false, message: 'Internal Server Error' }); 
+    }
+};
+
 
 
 module.exports = {
     registerUser,
+    loginUser
 };
